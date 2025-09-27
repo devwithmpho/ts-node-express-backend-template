2f9bedfa9724ebb7353fbfcdbc6f055356c7f044
devwithmpho <mphosomo07@gmail.com>
2025-09-27 12:49:26 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Server is running fine!')
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
