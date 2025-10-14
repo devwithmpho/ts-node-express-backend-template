8ea8f12ef3452c92f266c0490d82b3095881d0ea
devwithmpho <mphosomo07@gmail.com>
2025-10-14 04:39:14 +0200

    add Application type

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import express, { Request, Response } from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express, { Application, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Server is working!')
-})
+	res.send('Server is working!');
+});
 
 app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`)
-})
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
